07d067841d9f6aaa2f818a5e176818c2e97d1444
liangzhaoliang <liangzhaoliang95@163.com>
2024-12-30 13:14:48 +0800

    docs: 调整注释

--- java/src/main/java/org/InfiWebSdk/v1/Main.java
+++ java/src/main/java/org/InfiWebSdk/v1/Main.java
@@ -20,16 +20,16 @@ public class Main {
 
     public static void main(String[] args) {
         /*
-         * title:计算 InfiWebSdk 所需的token
+         * title: 计算 InfiWebSdk 所需的token
          * desc: 用于下发给前端InfiWebSdk中作为initToken参数使用
          * url: https://developer.infi.cn/docs/guide/prepare/initToken
          * @appId 为英飞分配的appId
          * @secret 为英飞分配的secret
-         * @recordId 为白板唯一ID,即上述接口创建获取到的recordId
+         * @recordId 为白板唯一ID,调用创建接口获取的recordId
          * @loginName 为白板唯一用户名，为对接系统中的用户唯一标识，后续相关回调以及白板中确定用户身份都会用到
          * @validTime 为签名有效期,单位为秒
          * */
-        String appId = "demo";
+        String appId = "1zDtJi";
         String secret = "3oGPeNTjHdvxX2h7tR35OfPVOqYJQrzI";
         String recordId = "recordId";
         String loginName = "loginName";
